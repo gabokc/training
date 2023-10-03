851ae3400463c4d549fb3e9a547249f30c497d8b
Gabriel Munera <gabriel.munera@kinandcarta.com>
2023-10-03 17:14:47 -0500

    Set up test execution

---
+++ events-producer/src/test/java/dev/gabrielmumo/kc/lab/kafka/controller/LibraryEventsControllerIT.java
@@ -0,0 +1,13 @@
+package dev.gabrielmumo.kc.lab.kafka.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsControllerIT {
+
+    @Test
+    public void test(){
+        System.out.println("< < < < < Runs Integration Test > > > > >");
+    }
+}
---
+++ events-producer/src/test/java/dev/gabrielmumo/kc/lab/kafka/controller/LibraryEventsControllerTest.java
@@ -0,0 +1,11 @@
+package dev.gabrielmumo.kc.lab.kafka.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class LibraryEventsControllerTest {
+
+    @Test
+    public void test(){
+        System.out.println("< < < < < Runs Unit Test > > > > >");
+    }
+}
