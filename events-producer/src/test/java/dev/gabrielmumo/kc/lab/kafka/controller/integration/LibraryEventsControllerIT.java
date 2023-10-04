3490900be0f8b14a7afb9ee3732fb21cd41a6e05
Gabriel Munera <gabriel.munera@kinandcarta.com>
2023-10-03 20:09:20 -0500

    Add Integration test

--- events-producer/src/test/java/dev/gabrielmumo/kc/lab/kafka/controller/integration/LibraryEventsControllerIT.java
+++ events-producer/src/test/java/dev/gabrielmumo/kc/lab/kafka/controller/integration/LibraryEventsControllerIT.java
@@ -1,13 +1,83 @@
 package dev.gabrielmumo.kc.lab.kafka.controller.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.gabrielmumo.kc.lab.kafka.domain.LibraryEvent;
+import dev.gabrielmumo.kc.lab.kafka.util.TestUtil;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = "library-events")
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
 class LibraryEventsControllerIT {
 
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Consumer<Integer, String> consumer;
+
+    @BeforeEach
+    public void setUp() {
+        var configs = new HashMap<>(KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker));
+        configs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        consumer = new DefaultKafkaConsumerFactory<>(configs, new IntegerDeserializer(), new StringDeserializer())
+                .createConsumer();
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        consumer.close();;
+    }
+
     @Test
-    public void test(){
-        System.out.println("< < < < < Runs Integration Test > > > > >");
+    public void postLibraryEvent(){
+        var httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        var libraryEventRequest =  TestUtil.libraryEventRecord();
+        var httpEntity = new HttpEntity<>(libraryEventRequest, httpHeaders);
+
+        var responseEntity = testRestTemplate.exchange(
+                "/v1/libraryevent",
+                HttpMethod.POST,
+                httpEntity,
+                LibraryEvent.class);
+
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+        ConsumerRecords<Integer, String> consumerRecords = KafkaTestUtils.getRecords(consumer);
+        assert consumerRecords.count() == 1;
+
+        consumerRecords.forEach(r -> {
+                var libraryEventMsg = TestUtil.parseLibraryEventRecord(objectMapper, r.value());
+                assertEquals(libraryEventRequest, libraryEventMsg);
+            }
+        );
     }
 }
---
+++ events-producer/src/test/java/dev/gabrielmumo/kc/lab/kafka/util/TestUtil.java
@@ -0,0 +1,26 @@
+package dev.gabrielmumo.kc.lab.kafka.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.gabrielmumo.kc.lab.kafka.domain.Book;
+import dev.gabrielmumo.kc.lab.kafka.domain.LibraryEvent;
+import dev.gabrielmumo.kc.lab.kafka.domain.LibraryEventType;
+
+public class TestUtil {
+
+    public static Book bookRecord(){
+        return new Book(123, "Kafka Using Spring Boot","Dilip" );
+    }
+
+    public static LibraryEvent libraryEventRecord(){
+        return new LibraryEvent(null, LibraryEventType.NEW, bookRecord());
+    }
+
+    public static LibraryEvent parseLibraryEventRecord(ObjectMapper objectMapper , String json){
+        try {
+            return  objectMapper.readValue(json, LibraryEvent.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
