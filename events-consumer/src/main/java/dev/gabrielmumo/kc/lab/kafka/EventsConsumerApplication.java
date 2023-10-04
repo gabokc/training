9d1387ca24b2b4c7187043c7d635ceeb65c28c99
Gabriel Munera <gabriel.munera@kinandcarta.com>
2023-10-04 08:30:39 -0500

    Events consumer setup

---
+++ events-consumer/src/main/java/dev/gabrielmumo/kc/lab/kafka/EventsConsumerApplication.java
@@ -0,0 +1,13 @@
+package dev.gabrielmumo.kc.lab.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsConsumerApplication.class, args);
+	}
+
+}
---
+++ events-consumer/src/test/java/dev/gabrielmumo/kc/lab/kafka/EventsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabrielmumo.kc.lab.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
